51b0946bbfff1a7faca6f9a774813d78a11cb48b
Joseph Joslin <jjoslin07@gmail.com>
2021-07-09 18:24:00 -0700

    Create a POST route for Testing
    Create a test POST route.
    
    npm install --save body-parser

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/test/post', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Thing created successfully!'
+    });
 });
 
 app.get('/api/test/get', (req, res, next) => {
