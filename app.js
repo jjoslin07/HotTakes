1c3706ba5f89fcdb2c38d35ef082992296436e89
Joseph Joslin <jjoslin07@gmail.com>
2021-07-18 12:12:37 -0700

    change database connection and .env file

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const app = express();
 require('dotenv').config();
 
 
-mongoose.connect(process.env.DB_CONNECTION, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fip4x.mongodb.net/projectSixDatabase?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => {
         console.log('Successfully connected to MongoDB Atlas!');
     })
